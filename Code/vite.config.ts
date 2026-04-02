d74722a6ad10eecd597226459b4b41bd87a0dea5
Alejandro Ramirez <alejandro.ramirez.v@ucb.edu.bo>
2026-04-02 01:21:07 -0400

    feat: configurando Handlebars

--- Code/vite.config.ts
+++ Code/vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      handlebars: 'handlebars/dist/handlebars.js',
+    },
+  },
+  optimizeDeps: {
+    include: ['handlebars'],
+  },
   server: {
     port: 5173,
     open: true,
